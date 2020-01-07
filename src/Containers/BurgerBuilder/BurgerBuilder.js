05f0d556c2ef523201ed11325b635bdb99f76c79
Billy <billyjcatchpole@gmail.com>
2020-01-07 18:31:46 +0000

    Started on rendering the burger ingredients and file structure along with project setup

---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const BurgerBuilder = () => {
+
+    return (
+
+        <React.Fragment>
+
+            <div>
+
+                Burger visuals
+            </div>
+
+            <div>
+
+                Build controls
+            </div>
+            
+        </React.Fragment>
+
+    )
+}
+
+export default BurgerBuilder
